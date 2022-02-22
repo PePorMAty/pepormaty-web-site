a89b2ade2555c46066f16a23041bd2b0643a7a82
PePorMaty <themranti3256@gmail.com>
2022-02-22 15:12:21 +0700

    The beginning of dialog layout

--- src/components/Pages/Messages/Messages.jsx
+++ src/components/Pages/Messages/Messages.jsx
@@ -1,9 +1,47 @@
+import { NavLink } from "react-router-dom";
 import s from "./Messages.module.css";
 
-const Messages=() => {
+const DialogItem = (props) =>{
+  
+  return (
+    <div className={s.item + ' ' + s.active}>
+      <NavLink className={s.itemLink} to={props.id}>{props.name}</NavLink>
+    </div>
+  );
+}
+const DialogMessage = (props) => {
+
   return (
     <div>
-      <div>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</div>
+    <div className={s.message__item}>{props.message}</div>
+    </div>
+  );
+}
+
+const Messages = (props) => {
+  return (
+    <div className={s.dialogs}>
+      <div className={s.bg}>
+        <div className={s.dialogs__item}>
+          <DialogItem name="Anton" id="1" />
+          <div className={s.item}>
+            <DialogItem name="Liza" id="2" />
+          </div>
+          <div className={s.item}>
+            <DialogItem name="Alina" id="3" />
+          </div>
+          <div className={s.item}>
+            <DialogItem name="Lena" id="4" />
+          </div>
+        </div>
+      </div>
+      <div className={s.bg}>
+        <div className={s.messages}>
+          <DialogMessage message="Hi" />
+          <DialogMessage message="How are you?" />
+          <DialogMessage message="Good" />
+        </div>
+      </div>
     </div>
   );
 }
